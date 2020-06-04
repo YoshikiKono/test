9cca15aa78004b2f0590091d922d996b2d98a312
YoshikiKono <yoshiki_kono@egxp.io>
2020-06-04 18:42:38 +0900

    Vuetify化してみた

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,32 +1,34 @@
 var app = new Vue({
   el: '#app',
+  vuetify: new Vuetify(),
   data: {
-      addText: '',
-      list: [],
-      absolute: 0,
+    addText: "",
+    list: [],
+    absolute: 0,
   },
   methods: {
-      addToDo() {
-          if (this.addText) {
-              this.list.unshift({
-                  'text': this.addText,
-                  'id': this.absolute + 1
-              });
-              this.addText = '';
-              this.absolute++;
-          }
-      },
-      deleteToDo(id) {
-          var deleteIndex = '';
-          var check = confirm('本当に削除しますか？');
-          if (check === true) { 
-              this.list.some(function (value, index) {
-                  if (value.id === id) {
-                      deleteIndex = index;
-                  }
-              });
-              this.list.splice(deleteIndex, 1);
-          }
-      },
+    addToDo: function(e) {
+      //var added = alert('予定の登録をします');
+      if (this.addText) {
+        this.list.unshift({
+            'text': this.addText,
+            'id': this.absolute + 1
+        });
+        this.addText = '';
+        this.absolute++;
+      }
+    },
+    deleteToDo(id) {
+      var deleteIndex = '';
+      var check = confirm('予定を削除しますか？');
+      if (check === true) { 
+          this.list.some(function (value, index) {
+              if (value.id === id) {
+                  deleteIndex = index;
+              }
+          });
+          this.list.splice(deleteIndex, 1);
+      }
+   },
   }
-});
+}).$mount('#app')
